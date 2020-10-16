9f925088f5170680c449059331b42af54f2a26fa
marco Koch <marco.koch@uni-saarland.de>
2020-10-16 17:26:07 +0200

    added axios

--- PHP/mysql.php
+++ PHP/mysql.php
@@ -5,10 +5,10 @@ $data = json_decode(file_get_contents('php://input'));
 
 var_export($data);
 
-$sql = $mysqli->prepare("INSERT INTO `Evadaten` (LSF, response) VALUES (?, ?)");
+$sql = $mysqli->prepare("INSERT INTO `LogfilesLisa` (VPCode, Grp, Log) VALUES (?, ?, ?)");
 
 
-$sql->bind_param("ss", $data->lsf, implode("\t", $data->response));
+$sql->bind_param("sis", $data->lsf, implode("\t", $data->response));
 
 $sql->execute();
 
