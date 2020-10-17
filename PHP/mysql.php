d416cdb5b8ed91e0d83b3a1bdb033f260d610c42
marco Koch <marco.koch@uni-saarland.de>
2020-10-17 17:19:06 +0200

    removed log from php

--- PHP/mysql.php
+++ PHP/mysql.php
@@ -3,10 +3,7 @@ include "config.php";
 
 $data = json_decode(file_get_contents('php://input'));
 
-var_export($data);
-
 $sql = $mysqli->prepare("INSERT INTO `LogfilesLisa` (VPCode, Grp, Log) VALUES (?, ?, ?)");
-
 
 $sql->bind_param("sis", $data->VPCode, $data->grp, $data->logData);
 
